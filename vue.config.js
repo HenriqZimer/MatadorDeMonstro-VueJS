327c92bd6bb948224889804cb5f6ee1b98739152
Henrique Zimermann <henriquezimermanngeo@gmail.com>
2023-07-26 20:53:37 -0300

    teste deploy

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/VueJs-teste-producao/' // aqui é onde você coloca a subpasta onde o site será hospedado
+    ? '/MatadorDeMonstroVueJS/' // aqui é onde você coloca a subpasta onde o site será hospedado
     : '/',
   transpileDependencies: true,
 })
