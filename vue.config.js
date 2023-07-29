0f505612f73a8d842103ddb4eeb51c72ec0b17ae
Henrique Zimermann <henriquezimermanngeo@gmail.com>
2023-07-28 22:01:37 -0300

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/MatadorDeMonstro-VueJS/' // aqui é onde você coloca a subpasta onde o site será hospedado
+    ? '/MatadorDeMonstro-VueJS/' 
     : '/',
   transpileDependencies: true,
 })
